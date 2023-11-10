4dc7321b0760a7d00388423f4385956568ee406f
ArthurV0n <arthur.pedroso@sptech.school>
2023-11-10 15:36:21 -0300

    Alterando a Dashboard e ajustando o modelo de BD

--- Banco de dados/Final/BD_V2.sql
+++ Banco de dados/Final/BD_V2.sql
@@ -1,5 +1,6 @@
 create database mushroom;
 use mushroom;
+drop database mushroom;
 
 -- Criação das Tabelas
 
@@ -72,21 +73,27 @@ insert into usuarios values
     (1001,'ADM', 'Paulo', '453.545.030-78', '*****',2,'paulo@email.com'),
     (1002,'Normal', 'João', '673.865.700-58', '*********',3,'joão@email.com');
     
-    insert into fase (faseCogumelo,tempMin,tempMax,umiMin,umiMax) values
-('Inoculação',20,30,50,73),
-('colonização',22,35,50,70),
-('desenvolvimento',20,37,52,70);
+    insert into fase (cogumelo,faseCogumelo,tempMin,tempMax,umiMin,umiMax) values
+('Champignom','Inoculação',20,30,50,73),
+('Champignom','Colonização',20,30,50,73),
+('Champignom','Desenvolvimento',20,30,50,73),
+('Shiitake','Inoculação',22,35,50,70),
+('Shiitake','colonização',22,35,50,70),
+('Shiitake','Desenvolvimento',22,35,50,70),
+('Shimeji','Inoculação',20,37,52,70),
+('Shimeji','Colonização',20,37,52,70),
+('Shimeji','desenvolvimento',20,37,52,70);
 
-    insert into setor (nome,fkEmpresa,cogumelo,fkFase) values
-	('setor 1',1,'champignon',1),
-    ('setor 2',1,'Shitake',2),
-    ('setor 3',1,'Shimeji',1),
-    ('setor 1',2,'champignon',1),
-    ('setor 2',2,'Shitake',1),
-    ('setor 1',3,'Shimeji',1),
-    ('setor 2',3,'champignon',2),
-    ('setor 3',3,'Shitake',3),
-    ('setor 4',3,'Shimeji',3);
+    insert into setor (nome,fkEmpresa,fkFase) values
+	('setor 1',1,1),
+    ('setor 2',1,6),
+    ('setor 3',1,1),
+    ('setor 1',2,1),
+    ('setor 2',2,1),
+    ('setor 1',3,1),
+    ('setor 2',3,6),
+    ('setor 3',3,9),
+    ('setor 4',3,9);
     
 insert into registros (dataHora, dadosUmi,dadosTemp,fkSetor,fkEmpresa) values
 (current_timestamp(),70,25,10,1),
@@ -94,7 +101,7 @@ insert into registros (dataHora, dadosUmi,dadosTemp,fkSetor,fkEmpresa) values
 (current_timestamp(),80,26,12,1),
 (current_timestamp(),70,28,13,2),
 (current_timestamp(),85,27,14,2),
-(current_timestamp(),83,25,15,3),
+(current_timestamp(),83,25,15,2),
 (current_timestamp(),82,27,16,3),
 (current_timestamp(),87,24,17,3),
 (current_timestamp(),89,25,18,3);
@@ -121,27 +128,27 @@ select empresa.nome as empresa, usuarios.nome as Funcionarios , usuarios.tipoUse
 on usuarios.fkEmpresa = empresa.idEmpresa;
 
 
--- Empresas Setores e seus cogumelos
+-- Empresas e Setores 
 
 
-select empresa.nome as empresa, setor.nome as setor, setor.cogumelo as cogumelo from empresa join setor
+select empresa.nome as empresa, setor.nome as setor from empresa join setor
 on setor.fkEmpresa = empresa.idEmpresa;
 
 
--- Determinadas Empresas com os registros de cada sensor
+-- Determinadas Empresas seus setores com os registros de seus sensor
 
 
-select empresa.nome as empresa, setor.nome as setor, setor.cogumelo as Cogumelo, registros.dadosUmi, registros.dadosTemp  from empresa join setor on 
+select empresa.nome as empresa, setor.nome as setor,  registros.dadosUmi, registros.dadosTemp  from empresa join setor on 
 setor.fkempresa = empresa.idempresa 
 join registros on registros.fksetor = setor.idsetor
 where empresa.nome ='vendetta Cogu';
 
-select empresa.nome as empresa, setor.nome as setor, setor.cogumelo as Cogumelo, registros.dadosUmi, registros.dadosTemp  from empresa join setor on 
+select empresa.nome as empresa, setor.nome as setor, registros.dadosUmi, registros.dadosTemp  from empresa join setor on 
 setor.fkempresa = empresa.idempresa 
 join registros on registros.fksetor = setor.idsetor
 where empresa.nome ='Delicias Cogumelos';
 
-select empresa.nome as empresa, setor.nome as setor, setor.cogumelo as Cogumelo, registros.dadosUmi, registros.dadosTemp  from empresa join setor on 
+select empresa.nome as empresa, setor.nome as setor, registros.dadosUmi, registros.dadosTemp  from empresa join setor on 
 setor.fkempresa = empresa.idempresa 
 join registros on registros.fksetor = setor.idsetor
 where empresa.nome ='Cogumelos Domingues';
@@ -150,15 +157,15 @@ where empresa.nome ='Cogumelos Domingues';
 -- Determinadas Empresas e as fases de cada setor
 
 
-select empresa.nome as empresa, setor.nome as setor, setor.cogumelo as Cogumelo, fase.fasecogumelo as fase from empresa join setor
+select empresa.nome as empresa, setor.nome as setor, fase.cogumelo as Cogumelo, fase.fasecogumelo as fase  from empresa join setor
 on setor.fkempresa = empresa.idempresa join fase on setor.fkfase = fase.idfase
 where empresa.nome ='vendetta Cogu' ;
 
-select empresa.nome as empresa, setor.nome as setor, setor.cogumelo as Cogumelo, fase.fasecogumelo as fase from empresa join setor
+select empresa.nome as empresa, setor.nome as setor, fase.cogumelo as Cogumelo, fase.fasecogumelo as fase from empresa join setor
 on setor.fkempresa = empresa.idempresa join fase on setor.fkfase = fase.idfase
 where empresa.nome ='Delicias Cogumelos' ;
 
-select empresa.nome as empresa, setor.nome as setor, setor.cogumelo as Cogumelo, fase.fasecogumelo as fase from empresa join setor
+select empresa.nome as empresa, setor.nome as setor, fase.cogumelo as Cogumelo, fase.fasecogumelo as fase from empresa join setor
 on setor.fkempresa = empresa.idempresa join fase on setor.fkfase = fase.idfase
 where empresa.nome ='Cogumelos Domingues' ;
 
